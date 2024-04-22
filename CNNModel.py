92f30d533959c1262c96a8c162ad2bf6d7617f75
MisinformedOwl <jaydean1311@gmail.com>
2024-04-22 14:27:40 +0100

    + Model finally, finally has good accuracy.

--- CNNModel.py
+++ CNNModel.py
@@ -25,9 +25,9 @@ We call nn.Module to make use of torch's pre built features and backprop.
 class CNNModel(nn.Module):
     history = []
     criterion = nn.MSELoss()
-    batchsize = 32
+    batchsize = 16
     learningRate = 0.001
-    testsize = 20 # in %
+    testsize = 10 # in %
     def __init__(self):
         super().__init__()
         '''
@@ -38,29 +38,7 @@ class CNNModel(nn.Module):
         print(self.device)
         self.to(self.device)
         
-        self.layer1 = nn.Conv2d(3, 10, kernel_size=3) # Collecting image of [512,512,3] and colecting featuresto create a matrix of [512,512,10]
-        self.layer2 = nn.MaxPool2d(2,2)              #Maxpool halves the size of the image. Image is now [256,256,10]
-        self.layer3 = nn.Conv2d(10, 20, kernel_size=3) #[256,256,10]->[256,256,20]
-        self.layer4 = nn.MaxPool2d(2,2)              #[256,256,3]->[128,128,3]
-        self.layer5 = nn.Conv2d(20, 40, kernel_size=3) #[128,128,20]->[128,128,20]
-        self.layer6 = nn.MaxPool2d(2,2)              #[128,128,20]->[64,64,20]
-        self.layer7 = nn.Conv2d(40, 40, kernel_size=3) #[64,64,20]->[64,64,10]
-        self.layer8 = nn.MaxPool2d(2,2)              #[64,64,10]->[32,32,10]
-        self.layer9 = nn.Conv2d(40, 20, kernel_size=3) #[32,32,10]->[32,32,10]
-        
-        self.norm = nn.BatchNorm2d(20)
-        
-        self.flatten = nn.Flatten()
-        
-        self.lin1 = nn.Linear(15680, 2048)            #124x124x1 = 15,376
-        self.drop1 = nn.Dropout(0.5)
-        self.lin2 = nn.Linear(2048, 256)
-        self.drop2 = nn.Dropout(0.5)
-        self.lin3 = nn.Linear(256, 32)               #Gradually reduce neurons until we reach 2, the cordinates.
-        self.lin4 = nn.Linear(32, 16)
-        self.lin5 = nn.Linear(16, 8)
-        self.lin6 = nn.Linear(8, 4)
-        self.lin7 = nn.Linear(4, 2)
+        self._SimpleModel()
         
         for m in self.modules():
             if isinstance(m, (nn.Conv2d, nn.Linear)):
@@ -74,8 +52,28 @@ class CNNModel(nn.Module):
             if isinstance(m, (nn.Conv2d, nn.Linear)):
                 assert m.weight.sum().item() != 0
         
-        self.optimizer = optim.Adam(self.parameters(), lr = self.learningRate) #It is unable to see parameters therefore must be established here.
+        self.optimizer = optim.SGD(self.parameters(), lr = self.learningRate, momentum=0.9) #It is unable to see parameters therefore must be established here.
+    
+    '''
+    Makes a simple version of the model.
+    '''
+    def _SimpleModel(self):
+        self.layer1 = nn.Conv2d(3, 10, kernel_size=3, padding=1) # Collecting image of [512,512,3] and colecting featuresto create a matrix of [512,512,10]
+        self.layer2 = nn.Conv2d(10, 10, kernel_size=3, padding=1) # Collecting image of [512,512,3] and colecting featuresto create a matrix of [512,512,10]
+        self.layer3 = nn.Conv2d(10, 10, kernel_size=3, padding=1) # Collecting image of [512,512,3] and colecting featuresto create a matrix of [512,512,10]
+        self.layer4 = nn.MaxPool2d(2,2)              #Maxpool halves the size of the image. Image is now [256,256,10]
+        self.layer5 = nn.Conv2d(10, 10, kernel_size=3, padding=1) #[256,256,10]->[256,256,20]
+        self.layer6 = nn.Conv2d(10, 10, kernel_size=3, padding=1) #[256,256,10]->[256,256,20]
         
+        self.norm = nn.BatchNorm2d(10)
+        
+        self.flatten = nn.Flatten()
+        
+        self.lin1 = nn.Linear(40960, 2048)            #124x124x1 = 15,376
+        self.drop1 = nn.Dropout(0.5)
+        self.lin2 = nn.Linear(2048, 256)
+        self.lin3 = nn.Linear(256, 2)
+    
     '''
     This function is what is activated when actually training/testing the AI.
     This is a useful tool in torch, as it allows me to intercept hidden layer
@@ -84,29 +82,21 @@ class CNNModel(nn.Module):
     '''
     def forward(self, x):
         s = []
-        x = F.sigmoid(self.layer1(x))
-        x = self.layer2(x)
-        x = F.sigmoid(self.layer3(x))
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
         x = self.layer4(x)
-        x = F.sigmoid(self.layer5(x))
-        x = self.layer6(x)
-        x = F.sigmoid(self.layer7(x))
-        x = self.layer8(x)
-        x = F.sigmoid(self.layer9(x))
+        x = F.relu(self.layer5(x))
+        x = F.relu(self.layer6(x))
         
         x = self.norm(x)
         
         x = self.flatten(x)
         
-        x = F.sigmoid(self.lin1(x))
+        x = F.relu(self.lin1(x))
         x = self.drop1(x)
-        x = F.sigmoid(self.lin2(x))
-        x = self.drop2(x)
-        x = F.sigmoid(self.lin3(x))
-        x = F.sigmoid(self.lin4(x))
-        x = F.sigmoid(self.lin5(x))
-        x = F.sigmoid(self.lin6(x))
-        x = self.lin7(x)
+        x = F.relu(self.lin2(x))
+        x = self.lin3(x)
         
         return x
     
@@ -119,6 +109,8 @@ class CNNModel(nn.Module):
     
     '''
     Grabs the data from file and puts it into a dataloader for use in the model.
+    
+    [This needs reworked to be better.]
     '''
     def grabData(self, datafolder, resizeData=True, test = False):
         if datafolder == None:
@@ -129,9 +121,8 @@ class CNNModel(nn.Module):
         if not os.path.isdir(self.location):
             raise FileNotFoundError
         
-        mn = 0
-        mx = 255
         images = glob.glob(f"{self.location}/*.png")
+        images = sorted(images, key = len)
         
         
         #Grabbing user inputs and locations.
@@ -139,10 +130,10 @@ class CNNModel(nn.Module):
             inputs = pickle.load(file)
         
         initialSize = len(inputs)
-        testIndex = round(len(inputs)/100)*self.testsize
+        testIndex = round((len(inputs)/100)*self.testsize)
         if test:
-            inputs = inputs[:testIndex+2]
-            images = images[:testIndex+2]
+            inputs = inputs[:testIndex]
+            images = images[:testIndex]
         else:
             inputs = inputs[testIndex:]
             images = images[testIndex:]
@@ -156,12 +147,13 @@ class CNNModel(nn.Module):
                 image = imread(image)
                 image = image /255
                 image = image.round(4)
-                assert (image > 2).sum() == 0
-                data.append([torch.tensor(np.transpose(resize(image, (512, 512), INTER_AREA), (2,0,1)), dtype=torch.float).to(self.device), torch.tensor(i[0]).to(self.device), i[1]])
+                assert (image > 1).sum() == 0
+                data.append([torch.tensor(np.transpose(resize(image, (128, 128), INTER_AREA), (2,0,1)), dtype=torch.float).to(self.device), torch.tensor(i[0]).to(self.device), i[1]])
         else:
             for image, i in zip(images, inputs):
                 data.append([torch.tensor(np.transpose(imread(image), (2,0,1)), dtype=torch.float).to(self.device), torch.tensor(i[0]).to(self.device), i[1]])
-            
+        
+        del images, image, i, initialSize, testIndex, inputs
         return DataLoader(data, batch_size=self.batchsize, shuffle=False)
 
     '''
@@ -182,6 +174,8 @@ class CNNModel(nn.Module):
                 self.optimizer.step()
                 runningloss += loss.item()/self.batchsize
             self.history.append(runningloss)
+        del trainloader, image, cords, button, y_pred, loss
+        torch.cuda.empty_cache()
         return runningloss
     
     def denormalise(self, image, cord):
@@ -224,4 +218,6 @@ class CNNModel(nn.Module):
                 plt.imshow(img)  # Display the numpy array directly
                 plt.axis('off')
                 plt.show()
+        del loader, image, cords, button, y_pred, loss
+        torch.cuda.empty_cache()
         return runningloss
